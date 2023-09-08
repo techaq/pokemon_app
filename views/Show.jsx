4d2f5dec54a803b358c6d1f87c798fbabf0e8c3e
Fatimah <aishafhonore@gmail.com>
2023-09-07 20:41:08 -0500

    Created show views of each pokemon

--- views/Show.jsx
+++ views/Show.jsx
@@ -1,20 +1,20 @@
-const React = require("react")
+const React = require("react");
 
 class Show extends React.Component {
-  render () {
-    const { name, img} = this.props.pokemon
+  render() {
+    const { name, img } = this.props.pokemon;
 
     return (
       <div>
         <h1> Gotta catch em all! </h1>
-        <h2> The {name} </h2>
-        <img src = {img} alt=""/>
+        <h2> {name.charAt(0).toUpperCase() + name.slice(1)}</h2>
+        <img src={img + ".jpg"} alt="" />
         <nav>
           <a href="/pokemon"> Back to Index Page </a>
         </nav>
       </div>
-    )
+    );
   }
 }
 
-module.exports = Show
+module.exports = Show;
